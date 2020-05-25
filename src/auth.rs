ecebb971681d8ff1a08c34f44d032959498daf5d
Milesq <wmilosz88@gmail.com>
2020-05-25 10:59:38 +0200

    Add bcrypt password hash

--- src/auth.rs
+++ src/auth.rs
@@ -1,12 +1,19 @@
 use bcrypt::{hash, DEFAULT_COST};
-use std::{collections::HashMap, thread};
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, sync::Arc, thread};
 use web_server::{HttpCode, Request, Response};
+
 mod register;
 
 macro_rules! json_err {
-    ($msg: expr) => {
-        "{\"err\": \"$msg\"}"
+    ($msg: tt) => {
+        concat!("{\"err\": \"", $msg, "\"}")
     };
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+struct User {
+    name: String,
 }
 
 pub fn register(req: Request, mut res: Response) -> Response {
@@ -17,7 +24,9 @@ pub fn register(req: Request, mut res: Response) -> Response {
         return res;
     }
 
-    let body: HashMap<String, String> = body.unwrap();
+    let body_arc: Arc<HashMap<String, String>> = Arc::new(body.unwrap());
+    let body = Arc::clone(&body_arc);
+
     let (user, pass) = (body.get("name".into()), body.get("pass".into()));
 
     if user.is_none() || pass.is_none() {
@@ -34,16 +43,23 @@ pub fn register(req: Request, mut res: Response) -> Response {
         return res;
     }
 
-    thread::spawn(move || {
+    let body = Arc::clone(&body_arc);
+    thread::spawn(move || -> Option<i32> {
+        let user = body.get("name".into())?;
+        let pass = body.get("pass".into())?;
+
         let hashed = hash(pass, DEFAULT_COST).unwrap();
         let register_result = register::save_user(user.to_string(), hashed);
 
-        log::trace!("Cannot register user");
-        log::error!("{:#?}", register_result.unwrap_err());
+        if let Err(e) = register_result {
+            log::trace!("Cannot register user");
+            log::error!("{:#?}", e);
+        }
+
+        None
     });
 
-    res.set_body(crate::private_key().as_str());
-    res
+    res.set_body("JWT")
 }
 
 pub fn login(_req: Request, res: Response) -> Response {
